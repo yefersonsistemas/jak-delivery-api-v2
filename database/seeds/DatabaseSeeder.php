15b681daac705ce16724250d018943b4a44be4e7
jefersonmujica <jefersonmujica@gmail.com>
2020-03-30 11:12:27 -0400

    bajando de ysbelia

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         $this->call(CityTableSeeder::class);
         $this->call(MunicipalityTableSeeder::class);
-        $this->call(ParisheTableSeeder::class);
+        // $this->call(ParisheTableSeeder::class);
         $this->call(StateTableSeeder::class);
     }
 }
--- database/seeds/StateTableSeeder.php
+++ database/seeds/StateTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\State;
 use Illuminate\Database\Seeder;
 
 class StateTableSeeder extends Seeder
@@ -11,6 +12,106 @@ class StateTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        State::truncate();
+        
+   // factory(State::class)->create([
+        //     'name' => 'Amazonas',
+        // ]);
+
+        // factory(State::class)->create([
+        //     'name' => 'Anzoátegui',
+        // ]);
+
+        // factory(State::class)->create([
+        //     'name' => 'Apure',
+        // ]);
+
+        // factory(State::class)->create([
+        //     'name' => 'Aragua',
+        // ]);
+
+        // factory(State::class)->create([
+        //     'name' => 'Barinas',
+        // ]);
+
+        // factory(State::class)->create([
+        //     'name' => 'Bolívar',
+        // ]);
+
+        // factory(State::class)->create([
+        //     'name' => 'Carabobo',
+        // ]);
+
+        // factory(State::class)->create([
+        //     'name' => 'Cojedes',
+        // ]);
+
+        // factory(State::class)->create([
+        //     'name' => 'Delta Amacuro',
+        // ]);
+
+        // factory(State::class)->create([
+        //     'name' => 'Dependencias Federales',
+        // ]);
+
+        // factory(State::class)->create([
+        //     'name' => 'Distrito Capital',
+        // ]);
+
+        // factory(State::class)->create([
+        //     'name' => 'Falcón',
+        // ]);
+
+        // factory(State::class)->create([
+        //     'name' => 'Guárico',
+        // ]);
+
+        factory(State::class)->create([
+            'name' => 'Lara',
+        ]);
+
+        // factory(State::class)->create([
+        //     'name' => 'Mérida',
+        // ]);
+
+        // factory(State::class)->create([
+        //     'name' => 'Miranda',
+        // ]);
+
+        // factory(State::class)->create([
+        //     'name' => 'Monagas',
+        // ]);
+
+        // factory(State::class)->create([
+        //     'name' => 'Nueva Esparta',
+        // ]);
+
+        // factory(State::class)->create([
+        //     'name' => 'Portuguesa',
+        // ]);
+
+        // factory(State::class)->create([
+        //     'name' => 'Sucre',
+        // ]);
+
+        // factory(State::class)->create([
+        //     'name' => 'Táchira',
+        // ]);
+
+        // factory(State::class)->create([
+        //     'name' => 'Trujillo',
+        // ]);
+
+        // factory(State::class)->create([
+        //     'name' => 'Vargas',
+        // ]);
+
+        // factory(State::class)->create([
+        //     'name' => 'Yaracuy',
+        // ]);
+
+        // factory(State::class)->create([
+        //     'name' => 'Zulia',
+        // ]);
     }
 }
